b39749b02e8322b3b13d7b55475907003fe67a6a
Learzhu <694125196@qq.com>
2019-07-12 16:50:02 +0800

    1.新建观察者类测试

---
+++ app/src/main/java/com/learzhu/baseframework/test/ATest.java
@@ -0,0 +1,18 @@
+package com.learzhu.baseframework.test;
+
+/**
+ * ATest.java是液总汇的类。
+ *
+ * @author Learzhu
+ * @version 2.0.0 2019-04-12 10:49
+ * @update Learzhu 2019-04-12 10:49
+ * @updateDes
+ * @include {@link }
+ * @used {@link }
+ */
+public class ATest {
+    static String test = null;
+    public static void main(String args[]){
+        test.isEmpty();
+    }
+}
---
+++ app/src/main/java/com/learzhu/baseframework/test/observer/ObserverPatternTest.java
@@ -0,0 +1,19 @@
+package com.learzhu.baseframework.test.observer;
+
+import java.util.Observable;
+
+/**
+ * ObserverPatternTest.java是液总汇的类。
+ *
+ * @author Learzhu
+ * @version 2.0.0 2019-04-11 09:49
+ * @update Learzhu 2019-04-11 09:49
+ * @updateDes
+ * @include {@link }
+ * @used {@link }
+ */
+public class ObserverPatternTest {
+    public static void main(String args[]){
+        Observable observable = new Observable();
+    }
+}
